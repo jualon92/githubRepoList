def90b8e6b9edb5f6cebe35fb50dc4cc1dce9e95
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-18 09:41:41 -0300

    add search repos input

--- apps/monorepo-example/src/app/components/home/home.component.ts
+++ apps/monorepo-example/src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RepoInputComponent } from './repo-input/repo-input.component';
+import { ReposDashboardComponent } from './repos-dashboard/repos-dashboard.component';
 
 @Component({
   selector: 'angular-monorepo-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RepoInputComponent, ReposDashboardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- apps/monorepo-example/src/app/components/home/repo-input/repo-input.component.ts
+++ apps/monorepo-example/src/app/components/home/repo-input/repo-input.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MyErrorStateMatcher } from './validator';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'angular-monorepo-repo-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule , MatButtonModule ],
   templateUrl: './repo-input.component.html',
   styleUrl: './repo-input.component.scss',
 })
-export class RepoInputComponent {}
+export class RepoInputComponent {
+  userUrl:string = "";
+  repoFormControl = new FormControl('', [Validators.required]);
+  matcher = new MyErrorStateMatcher();
+
+  constructor() {}
+
+  searchRepo($event: Event) {
+    if (this.repoFormControl.invalid) {
+      return;
+    }
+  }
+}
