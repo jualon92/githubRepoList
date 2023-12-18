d754b63ac085cfdd3ba3b94b9f66ca9f2845d7df
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-18 06:57:02 -0300

    add header template

--- apps/monorepo-example/src/app/components/header/header.component.ts
+++ apps/monorepo-example/src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'angular-monorepo-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeLang(lang: string) {
+    console.log(lang);
+  }
+}
