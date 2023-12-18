137d7804390b66b86a0ea7523742cc3b60c18fa5
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-18 07:20:33 -0300

    add header and translate button

--- apps/monorepo-example/src/app/components/header/header.component.ts
+++ apps/monorepo-example/src/app/components/header/header.component.ts
@@ -3,21 +3,27 @@ import { CommonModule } from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { languages } from './header.entity';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'angular-monorepo-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule],
+  imports: [MatIconModule, TranslateModule, CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
-  constructor() { }
+  readonly languages = languages;
+  constructor( public translateService: TranslateService,  private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   changeLang(lang: string) {
-    console.log(lang);
+      //TODO: should be done with env
+      this.translateService.use(lang);
+      this._snackBar.open("Lang has been changed", "ok");
   }
 }
---
+++ apps/monorepo-example/src/app/components/header/header.entity.ts
@@ -0,0 +1,6 @@
+
+export enum languages {
+  ES = 'es',
+  EN = 'en'
+}
+
