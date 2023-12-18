243b7088e2531733459ce89597454d6888c6c0fa
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-18 09:07:33 -0300

    add routing styles

--- apps/monorepo-example/src/app/app.component.ts
+++ apps/monorepo-example/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule,  HeaderComponent ],
+  imports: [RouterModule,HomeComponent,  HeaderComponent  ],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/monorepo-example/src/app/app.routes.ts
+++ apps/monorepo-example/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+  /*
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('@monorepo-example/feature-about').then(
+        (m) => m.FeatureAboutModule
+      ),
+  }, */
+];
