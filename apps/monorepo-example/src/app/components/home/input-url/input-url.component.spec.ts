3aef74702b35666d8304689b3aed4c771afe50f8
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-18 12:02:52 -0300

    fix test imports error

--- apps/monorepo-example/src/app/components/home/input-url/input-url.component.spec.ts
+++ apps/monorepo-example/src/app/components/home/input-url/input-url.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputUrlComponent } from './input-url.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('InputUrlComponent', () => {
+fdescribe('InputUrlComponent', () => {
   let component: InputUrlComponent;
   let fixture: ComponentFixture<InputUrlComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InputUrlComponent],
+      imports: [InputUrlComponent, ReactiveFormsModule,   TranslateModule.forRoot(), BrowserAnimationsModule],
+      providers: [TranslateService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(InputUrlComponent);
@@ -15,7 +19,7 @@ describe('InputUrlComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component!!', () => {
     expect(component).toBeTruthy();
   });
 });
